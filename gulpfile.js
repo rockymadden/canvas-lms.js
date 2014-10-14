878f79f136802cd940552f84cdf59bdd15c0adc7
Rocky Madden <git@rockymadden.com>
2014-10-14 09:00:25 -0600

    Moved from grunt to gulp.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var coffee = require('gulp-coffee');
+var del = require('del');
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var paths = {
+  build: ['build'],
+  sources: ['src/coffeescript/**/*.coffee'],
+  tests: ['test/coffeescript/**/*.coffee']
+};
+
+
+gulp.task('clean', function(cb) {
+  del(paths.build, cb);
+});
+
+gulp.task('sources', ['clean'], function() {
+  return gulp.src(paths.sources)
+    .pipe(sourcemaps.init())
+    .pipe(coffee())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('test', ['sources', 'tests'], function() {
+  return gulp.src(['build/test/**/*.js'], {read: false}).pipe(mocha({reporter: 'spec', timeout: 32000}));
+});
+
+gulp.task('tests', ['clean'], function() {
+  return gulp.src(paths.tests)
+    .pipe(sourcemaps.init())
+    .pipe(coffee())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/test'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sources, ['sources']);
+  gulp.watch(paths.tests, ['tests']);
+});
+
+gulp.task('build', ['sources', 'tests']);
+
+gulp.task('default', ['build']);
